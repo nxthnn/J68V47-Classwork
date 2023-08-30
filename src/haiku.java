e794d3dc16c26da79e448d70b017f098f8b3fd48
nxthnn <nathanmorgan0912@gmail.com>
2023-08-30 15:40:14 +0100

    Haiku

---
+++ src/haiku.java
@@ -0,0 +1,7 @@
+public class haiku {
+    public static void main(String[] args) {
+        System.out.println("Chases down the pitch");
+        System.out.println("Striker approaches the box");
+        System.out.println("Shoots and misses wide");
+    }
+}
