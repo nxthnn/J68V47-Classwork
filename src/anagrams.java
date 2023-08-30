bb02cdebe16b7d18ebbdb04691507450e3049c8a
nxthnn <nathanmorgan0912@gmail.com>
2023-08-30 15:53:02 +0100

    anagrams

---
+++ src/anagrams.java
@@ -0,0 +1,9 @@
+public class anagrams {
+    public static void main(String[] args) {
+        System.out.println("Cheat cool = Chocolate");
+        System.out.println("Bites ilia = Abilities");
+        System.out.println("Chen quite = Technique");
+        System.out.println("Eris ET exp = Expertise");
+        System.out.println("Ashe flick = Lifehacks");
+    }
+}
