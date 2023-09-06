4c167a531adb721bca3bfc17d77c86c6d4e331ae
nxthnn <nathanmorgan0912@gmail.com>
2023-09-06 13:52:24 +0100

    Ex1

---
+++ src/Ex1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; // import the Scanner class 
+
+class Ex1 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+
+        // Enter username and press Enter
+        System.out.println("Enter name and hobby");
+        String name = myObj.nextLine();
+        String hobby = myObj.nextLine();
+
+        System.out.println("My friend " + name + " was a big fan of " + hobby +" he could do it all day and all night, he loved it so much that he got told he had to stop " + hobby + " and find a new thing to do.");
+    }
+}
