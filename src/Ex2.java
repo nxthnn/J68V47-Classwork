7ef859309bee0b6faa04c0e6b0a71f02a386ae02
nxthnn <nathanmorgan0912@gmail.com>
2023-09-06 14:13:38 +0100

    Ex2

---
+++ src/Ex2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Ex2 {
+
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        System.out.println("Can you enter number1 and number2");
+        int number1 = myObj.nextInt();
+        int number2 = myObj.nextInt();
+        System.out.println(number1 + number2);
+        System.out.println(number1 - number2);
+        System.out.println(number1 * number2);
+        System.out.println(number1 / number2);
+
+
+    }
+}
