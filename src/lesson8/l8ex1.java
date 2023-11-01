db9e81f9ea51371c4528db734f12f5940611c053
nxthnn <nathanmorgan0912@gmail.com>
2023-11-01 14:30:49 +0000

    Ex3

---
+++ src/lesson8/l8ex1.java
@@ -0,0 +1,17 @@
+package lesson8;
+
+public class l8ex1 {
+
+    public static void main(String[] args) {
+
+        String[] firstName = {"Ben", "Matty", "Alex", "Nathan", "Nathan"};
+        String[] lastName = {"Williamson", "Siwiec", "Wall", "Garden", "Morgan"};
+
+        System.out.println(firstName[0]+" "+lastName[0]);
+        System.out.println(firstName[1]+" "+lastName[1]);
+        System.out.println(firstName[2]+" "+lastName[2]);
+        System.out.println(firstName[3]+" "+lastName[3]);
+        System.out.println(firstName[4]+" "+lastName[4]);
+        System.out.println( firstName.length + lastName.length);
+    }
+}
