14854cf362ed6cde966b673734f7f519e82f4310
nxthnn <nathanmorgan0912@gmail.com>
2024-01-29 22:32:03 +0000

    Ex3

--- src/assessment/main.java
+++ src/assessment/main.java
@@ -1,7 +1,6 @@
 package assessment;
 
 import java.util.Scanner;
-import java.util.Random;
 import java.util.Map;
 import java.util.HashMap;
 public class main {
@@ -17,14 +16,33 @@ public class main {
         System.out.print("Pick your team. \nCeltic\nRangers\nAberdeen\nHearts\nKilmarnock\nSt.Mirren\nHIbernian\nDundee\nSt.Johnstone\nRoss County\nLivingston\n:");
         userTeam = scanner.nextLine();
 
-        if(!teams.containsKey(userTeam)) {
+        if (!teams.containsKey(userTeam)) {
             System.out.println("Invalid team name");
         } else {
             System.out.println("Selected team: " + userTeam);
         }
 //This ensures that the team you select is from the text file
-    }
+        int gamesPlayed = 0;
+        while (gamesPlayed < 11)
+        System.out.print("Do you want to train (Y/N): ");
+        String trainingInput = scanner.nextLine();
 
+        if (trainingInput.equalsIgnoreCase("Y")) {
+            int currentLevel = teams.get(userTeam);
+            int trainingEffectiveness = (int) (Math.random() * 11) - 5; // Random number between -5 and 5
+            int newLevel = currentLevel + trainingEffectiveness;
+            teams.put(userTeam, newLevel);
+            System.out.println("Your team rating is now " + newLevel);
+        } else if (trainingInput.equalsIgnoreCase("N")) {
+            int currentLevel = teams.get(userTeam);
+            System.out.println("Your team did not train so your rating stayed at " + currentLevel);
+        } else {
+            System.out.println("Invalid input. Please enter Y or N.");
+        }
+
+
+        gamesPlayed = gamesPlayed + 1;
+    }
     private static void initializeTeams() {
         teams.put("Celtic", 90);
         teams.put("Rangers", 85);
